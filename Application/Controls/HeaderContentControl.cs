60d3f43328351adb9223b27ef534221fde751879
JohnSmithDr <johnsmith1st@live.com>
2015-05-17 10:57:49 +0800

    HeaderContentControl

---
+++ Application/Controls/HeaderContentControl.cs
@@ -0,0 +1,42 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace JohnSmithDr.Application.Controls
+{
+    public class HeaderContentControl : ContentControl
+    {
+        #region public object Header
+
+        public object Header
+        {
+            get { return (object)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register(
+                "Header",
+                typeof(object),
+                typeof(HeaderContentControl),
+                new PropertyMetadata(null));
+
+        #endregion
+
+        #region public DataTemplate HeaderTemplate
+
+        public DataTemplate HeaderTemplate
+        {
+            get { return (DataTemplate)GetValue(HeaderTemplateProperty); }
+            set { SetValue(HeaderTemplateProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderTemplateProperty =
+            DependencyProperty.Register(
+                "HeaderTemplate",
+                typeof(DataTemplate),
+                typeof(HeaderContentControl),
+                new PropertyMetadata(null));
+
+        #endregion
+    }
+}
