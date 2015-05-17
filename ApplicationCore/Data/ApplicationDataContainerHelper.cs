76e13306ef4d47d0947b7548c369c09ba40ff109
JohnSmithDr <johnsmith1st@live.com>
2015-05-17 10:27:01 +0800

    api for data

---
+++ ApplicationCore/Data/ApplicationDataContainerHelper.cs
@@ -0,0 +1,26 @@
+﻿using Windows.Storage;
+
+namespace JohnSmithDr.ApplicationCore.Data
+{
+    public static class ApplicationDataContainerHelper
+    {
+        public static void Load<TRef>(
+            this ApplicationDataContainer container,
+            ref TRef localValue,
+            string settingKey)
+        {
+            if (container.Values.ContainsKey(settingKey))
+            {
+                localValue = (TRef)container.Values[settingKey];
+            }
+        }
+
+        public static void Save<TRef>(
+            this ApplicationDataContainer container,
+            TRef localValue,
+            string settingKey)
+        {
+            container.Values[settingKey] = localValue;
+        }
+    }
+}
---
+++ ApplicationCore/Data/JsonConverter.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace JohnSmithDr.ApplicationCore.Data
+{
+    public static class JsonConverter
+    {
+        public static string Serialize<T>(T value)
+        {
+            string result = null;
+            var settings = new DataContractJsonSerializerSettings
+            {
+                UseSimpleDictionaryFormat = true
+            };
+            var api = new DataContractJsonSerializer(value.GetType(), settings);
+            using (var stream = new MemoryStream())
+            {
+                api.WriteObject(stream, value);
+                stream.Seek(0, SeekOrigin.Begin);
+                var reader = new StreamReader(stream);
+                result = reader.ReadToEnd();
+            }
+            return result;
+        }
+
+        public static T Deserialize<T>(string text)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var writer = new StreamWriter(stream);
+                writer.Write(text);
+                writer.Flush();
+                stream.Seek(0, SeekOrigin.Begin);
+                var api = new DataContractJsonSerializer(typeof(T));
+                var obj = api.ReadObject(stream);
+                return (T)obj;
+            }
+        }
+    }
+}
