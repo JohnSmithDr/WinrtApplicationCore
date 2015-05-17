e49c7c8cb3871f62531e9d3063758992bffbf27d
JohnSmithDr <johnsmith1st@live.com>
2015-05-17 10:46:40 +0800

    behaviors

---
+++ Application/Behaviors/EnumVisualStateBehavior.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace JohnSmithDr.Application.Behaviors
+{
+    public class EnumVisualStateBehavior : DependencyObject, IBehavior
+    {
+        #region IBehavior
+
+        public DependencyObject AssociatedObject { get; private set; }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            var control = associatedObject as Control;
+
+            if (control == null)
+                throw new ArgumentException(
+                    "EnumVisualStateBehavior can only be attached to Control");
+
+            AssociatedObject = associatedObject;
+        }
+
+        public void Detach()
+        {
+            AssociatedObject = null;
+        }
+
+        #endregion
+
+        #region public bool UseTransitions
+
+        public bool UseTransitions
+        {
+            get { return (bool)GetValue(UseTransitionsProperty); }
+            set { SetValue(UseTransitionsProperty, value); }
+        }
+
+        public static readonly DependencyProperty UseTransitionsProperty =
+            DependencyProperty.Register(
+                "UseTransitions",
+                typeof(bool),
+                typeof(EnumVisualStateBehavior),
+                new PropertyMetadata(true));
+
+        #endregion
+
+        #region public object State
+
+        public object State
+        {
+            get { return (object)GetValue(StateProperty); }
+            set { SetValue(StateProperty, value); }
+        }
+
+        public static readonly DependencyProperty StateProperty =
+            DependencyProperty.Register(
+                "State",
+                typeof(object),
+                typeof(EnumVisualStateBehavior),
+                new PropertyMetadata(null, OnStatePropertyChanged));
+
+        private static void OnStatePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var source = d as EnumVisualStateBehavior;
+            source.OnStateChanged((object)e.OldValue, (object)e.NewValue);
+        }
+
+        private void OnStateChanged(object oldValue, object newValue)
+        {
+            if (this.AssociatedObject != null && newValue != null)
+            {
+                VisualStateManager.GoToState(this.AssociatedObject as Control, newValue.ToString(), true);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Application/Behaviors/SynchronizeSelectedItemsBehavior.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace JohnSmithDr.Application.Behaviors
+{
+    public class SynchronizeSelectedItemsBehavior : DependencyObject, IBehavior
+    {
+        private ListViewBase _view;
+        private INotifyCollectionChanged _data;
+        private bool _synchronizing;
+
+        #region public object SelectedItems
+
+        public object SelectedItems
+        {
+            get { return (object)GetValue(SelectedItemsProperty); }
+            set { SetValue(SelectedItemsProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectedItemsProperty =
+            DependencyProperty.Register(
+                "SelectedItems",
+                typeof(object),
+                typeof(SynchronizeSelectedItemsBehavior),
+                new PropertyMetadata(null, SelectedItemsPropertyChanged));
+
+        private static void SelectedItemsPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var obj = d as SynchronizeSelectedItemsBehavior;
+            obj.SelectedItemsPropertyChanged(e.OldValue, e.NewValue);
+        }
+
+        private void SelectedItemsPropertyChanged(object oldValue, object newValue)
+        {
+            if (_data != null)
+            {
+                _data.CollectionChanged -= OnSourceCollectionChanged;
+                _data = null;
+            }
+
+            if (newValue is INotifyCollectionChanged)
+            {
+                _data = newValue as INotifyCollectionChanged;
+                _data.CollectionChanged += OnSourceCollectionChanged;
+            }
+        }
+
+        #endregion
+
+        #region public bool EnableSynchronization
+
+        public bool EnableSynchronization
+        {
+            get { return (bool)GetValue(EnableSynchronizationProperty); }
+            set { SetValue(EnableSynchronizationProperty, value); }
+        }
+
+        public static readonly DependencyProperty EnableSynchronizationProperty =
+            DependencyProperty.Register(
+                "EnableSynchronization",
+                typeof(bool),
+                typeof(SynchronizeSelectedItemsBehavior),
+                new PropertyMetadata(true));
+
+        #endregion
+
+        #region IBehavior
+
+        public DependencyObject AssociatedObject
+        {
+            get { return _view; }
+        }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            if (associatedObject is ListViewBase == false)
+            {
+                return;
+            }
+
+            _view = associatedObject as ListViewBase;
+            _view.SelectionChanged += OnListViewSelectionChanged;
+        }
+
+        public void Detach()
+        {
+            if (_data != null)
+            {
+                _data.CollectionChanged -= OnSourceCollectionChanged;
+            }
+
+            _view.SelectionChanged -= OnListViewSelectionChanged;
+            _view = null;
+        }
+
+        #endregion
+
+        private void OnListViewSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (_synchronizing)
+            {
+                return;
+            }
+
+            _synchronizing = true;
+
+            if (_data is IList)
+            {
+                var list = _data as IList;
+                e.AddedItems.ForEach(i => list.Add(i));
+                e.RemovedItems.ForEach(i => list.Remove(i));
+            }
+
+            _synchronizing = false;
+        }
+
+        private void OnSourceCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (_synchronizing)
+            {
+                return;
+            }
+
+            _synchronizing = true;
+
+            try
+            {
+                var action = e.Action;
+                switch (action)
+                {
+                    case NotifyCollectionChangedAction.Add:
+                        foreach (var i in e.NewItems)
+                        {
+                            _view.SelectedItems.Add(i);
+                        }
+                        break;
+                    case NotifyCollectionChangedAction.Remove:
+                        foreach (var i in e.OldItems)
+                        {
+                            _view.SelectedItems.Remove(i);
+                        }
+                        break;
+                    case NotifyCollectionChangedAction.Reset:
+                        if (_view.SelectedItems.Count > 0)
+                        {
+                            _view.SelectedItems.Clear();
+                        }
+                        if (_data is IEnumerable)
+                        {
+                            foreach (var i in _data as IEnumerable)
+                            {
+                                _view.SelectedItems.Add(i);
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                
+            }
+
+            _synchronizing = false;
+        }
+    }
+}
